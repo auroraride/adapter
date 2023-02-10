3f0ef2165d180d47dd2d9d732dcf8b997ad892fa
liasica <magicrolan@qq.com>
2023-02-10 08:21:05 +0800

    缺失文件

---
+++ message/message.go
@@ -0,0 +1,11 @@
+// Copyright (C) liasica. 2023-present.
+//
+// Created at 2023-01-03
+// Based on adapter by liasica, magicrolan@qq.com.
+
+package message
+
+type Messenger interface {
+    MarshalBinary() ([]byte, error)
+    UnmarshalBinary(data []byte) error
+}
