820b3c3b3b13e51cbeaf3d6c50e3c469c35999f4
liasica <magicrolan@qq.com>
2023-02-17 22:54:53 +0800

    增加安全关闭channel和安全发送channel

---
+++ ch.go
@@ -0,0 +1,31 @@
+// Copyright (C) liasica. 2023-present.
+//
+// Created at 2023-02-17
+// Based on adapter by liasica, magicrolan@qq.com.
+
+package adapter
+
+func ChSafeClose[T any](ch chan T) (justClosed bool) {
+    defer func() {
+        if recover() != nil {
+            // The return result can be altered
+            // in a defer function call.
+            justClosed = false
+        }
+    }()
+
+    // assume ch != nil here.
+    close(ch)   // panic if ch is closed
+    return true // <=> justClosed = true; return
+}
+
+func ChSafeSend[T any](ch chan T, value T) (closed bool) {
+    defer func() {
+        if recover() != nil {
+            closed = true
+        }
+    }()
+
+    ch <- value  // panic if ch is closed
+    return false // <=> closed = false; return
+}
