31da8f13ff5c350ba19005ac52ea406df1038bd2
liasica <magicrolan@qq.com>
2023-04-10 10:32:42 +0800

    更新电池编码测试

--- battery_test.go
+++ battery_test.go
@@ -12,12 +12,12 @@ import (
 )
 
 func TestParseBatterySN(t *testing.T) {
-    bat, err := ParseBatterySN("XCB0862022110265")
+    bat, err := ParseBatterySN("XCB1191023030243")
     require.NoError(t, err)
-    target := &Battery{
+    target := Battery{
         Brand: BatteryBrandXC,
-        Model: "72V30AH",
-        SN:    "XCB0862022110265",
+        Model: "72V40AH",
+        SN:    "XCB1191023030243",
     }
     t.Logf("bat: %#v", bat)
 
@@ -27,7 +27,7 @@ func TestParseBatterySN(t *testing.T) {
 
     bat, err = ParseBatterySN("BT106002512NNTB211118182")
     require.NoError(t, err)
-    target = &Battery{
+    target = Battery{
         Brand: BatteryBrandTB,
         Model: "60V25AH",
         SN:    "BT106002512NNTB211118182",
