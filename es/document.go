4487627f2584deea1e2ca7f31aa0c5d99bd66966
liasica <magicrolan@qq.com>
2024-10-11 14:04:51 +0800

    创建文档调整

--- es/document.go
+++ es/document.go
@@ -6,9 +6,7 @@ package es
 
 import (
 	"context"
-	"time"
 
-	jsoniter "github.com/json-iterator/go"
 	"go.uber.org/zap"
 )
 
@@ -16,21 +14,10 @@ const (
 	DefaultFieldTimestamp = "@timestamp"
 )
 
-type Document interface {
-	GetTimestamp() time.Time
-}
-
 // CreateDocument 创建文档
-func (e *Elastic) CreateDocument(doc Document) {
+func (e *Elastic) CreateDocument(doc any) {
 	index := e.GetIndex()
-	b, _ := jsoniter.Marshal(doc)
-	a := make(map[string]any)
-	_ = jsoniter.Unmarshal(b, &a)
-	if _, ok := a[DefaultFieldTimestamp]; !ok {
-		a[DefaultFieldTimestamp] = doc.GetTimestamp()
-	}
-
-	res, err := e.client.Index(index).Document(a).Do(context.Background())
+	res, err := e.client.Index(index).Document(doc).Do(context.Background())
 	if err != nil {
 		zap.L().Error("document创建失败", logTag(), zap.Error(err), zap.String("index", index), zap.Reflect("payload", res))
 	}
