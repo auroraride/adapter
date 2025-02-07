815b96d0630ce29f75ea5a157d8262a7ecfdb7d0
liasica <magicrolan@qq.com>
2025-02-07 13:50:24 +0800

    修改memcache记录仓位信息

--- mem/cache.go
+++ mem/cache.go
@@ -6,6 +6,7 @@ package mem
 
 import (
 	"context"
+	"strconv"
 	"sync"
 
 	"github.com/redis/go-redis/v9"
@@ -50,29 +51,34 @@ func (m *Memcache) ClearCabinetBatteryCache(ctx context.Context, serial string) 
 }
 
 // AddCabinetBatteryCache 添加电柜电池缓存
-func (m *Memcache) AddCabinetBatteryCache(ctx context.Context, serial string, batterySn string) {
+func (m *Memcache) AddCabinetBatteryCache(ctx context.Context, serial string, ordinal int, batterySn string) {
 	if batterySn == "" {
 		return
 	}
-	m.db.SAdd(ctx, m.CabinetBatteryCacheKey(serial), batterySn)
+	m.db.HSet(ctx, m.CabinetBatteryCacheKey(serial), batterySn, ordinal)
 }
 
 // RemoveCabinetBatteryCache 移除电柜电池缓存
 func (m *Memcache) RemoveCabinetBatteryCache(ctx context.Context, serial string, batterySn string) {
-	m.db.SRem(ctx, m.CabinetBatteryCacheKey(serial), batterySn)
+	m.db.HDel(ctx, m.CabinetBatteryCacheKey(serial), batterySn)
 }
 
 // ListCabinetBatteryCache 获取电柜电池缓存
-func (m *Memcache) ListCabinetBatteryCache(ctx context.Context, serial string) []string {
-	return m.db.SMembers(ctx, m.CabinetBatteryCacheKey(serial)).Val()
+func (m *Memcache) ListCabinetBatteryCache(ctx context.Context, serial string) (list map[string]int) {
+	list = make(map[string]int)
+	val := m.db.HGetAll(ctx, m.CabinetBatteryCacheKey(serial)).Val()
+	for k, v := range val {
+		list[k], _ = strconv.Atoi(v)
+	}
+	return
 }
 
 // IsMemberCabinetBatteryCache 电池是否在电柜缓存中
 func (m *Memcache) IsMemberCabinetBatteryCache(ctx context.Context, serial string, batterySn string) bool {
-	return m.db.SIsMember(ctx, m.CabinetBatteryCacheKey(serial), batterySn).Val()
+	return m.db.HExists(ctx, m.CabinetBatteryCacheKey(serial), batterySn).Val()
 }
 
 // CountCabinetBatteryCache 电柜电池缓存数量
 func (m *Memcache) CountCabinetBatteryCache(ctx context.Context, serial string) int64 {
-	return m.db.SCard(ctx, m.CabinetBatteryCacheKey(serial)).Val()
+	return m.db.HLen(ctx, m.CabinetBatteryCacheKey(serial)).Val()
 }
--- mem/cache_test.go
+++ mem/cache_test.go
@@ -4,10 +4,48 @@
 
 package mem
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/require"
+)
 
 func TestCache(t *testing.T) {
-	s1 := Cache()
-	s2 := Cache()
-	t.Logf("s1: %p, s2: %p", s1, s2)
+	db := redis.NewClient(&redis.Options{
+		Addr: "127.0.0.1:6379",
+		DB:   15,
+	})
+	CacheSetup(db)
+
+	c1 := Cache()
+	c2 := Cache()
+	t.Logf("c1: %p, s2: %p", c1, c2)
+	require.Equal(t, c1, c2)
+
+	serial := "KHD40-19040000"
+
+	items := map[string]int{
+		"XCB1262023030318": 1,
+		"XCB1262023080582": 2,
+		"XCB1262023041836": 3,
+		"XCB1262023041464": 4,
+		"XCB1262023052066": 5,
+		"XCB1262023030678": 6,
+	}
+
+	ctx := context.Background()
+
+	for sn, ordinal := range items {
+		c1.AddCabinetBatteryCache(ctx, serial, ordinal, sn)
+	}
+
+	require.Equal(t, c1.CountCabinetBatteryCache(ctx, serial), int64(len(items)))
+	require.Equal(t, c1.IsMemberCabinetBatteryCache(ctx, serial, "XCB1262023030318"), true)
+	c1.RemoveCabinetBatteryCache(ctx, serial, "XCB1262023030318")
+	require.Equal(t, c1.IsMemberCabinetBatteryCache(ctx, serial, "XCB1262023030318"), false)
+	m := c1.ListCabinetBatteryCache(ctx, serial)
+	t.Log(m)
+	require.Equal(t, m["XCB1262023030678"], items["XCB1262023030678"])
 }
